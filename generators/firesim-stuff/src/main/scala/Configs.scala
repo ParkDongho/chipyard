a27cc4ef93685cf44c808eb9997d419251bf5d20
abejgonzalez <abe.j.gonza@gmail.com>
2024-08-08 10:49:52 -0700

    First commit

---
+++ generators/firesim-stuff/src/main/scala/Configs.scala
@@ -0,0 +1,10 @@
+//See LICENSE for license details.
+package firesim.configs
+
+import org.chipsalliance.cde.config.Config
+import firesim.bridges.{LoopbackNIC}
+
+// Enables NIC loopback the NIC widget
+class WithNICWidgetLoopback  extends Config((site, here, up) => {
+  case LoopbackNIC => true
+})
